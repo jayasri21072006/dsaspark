48a90b7689ea92208dae6b552a6635a4e5c04134
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-25 13:07:42 +0530

    Update javacodes.java

--- javacodes.java
+++ javacodes.java
@@ -167,3 +167,49 @@ class Main {
  *****
   ***
    *
+
+
+
+
+
+
+
+
+    You are given a number n. You need to generate and print a pattern based on the given value of n.
+
+For each row, starting from the first, print numbers in descending order from n down to 1.
+Each number in a row is repeated as many times as the current row index (starting from n).
+Instead of printing each row on a new line, separate rows with -1.
+Instead of a newline at the end of each row, print -1 to indicate row separation. After printing the entire pattern, end the output with -1.
+
+For n= 3,
+pattern:  3 3 3 2 2 2 1 1 1
+                    3 3 2 2 1 1 
+                    3 2 1
+
+
+Input: 2
+Output: [2, 2, 1, 1, -1, 2, 1, -1]
+Input: 3
+Output: [3, 3, 3, 2, 2, 2, 1, 1, 1, -1, 3, 3, 2, 2, 1, 1, -1, 3, 2, 1, -1]
+
+
+
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> printPat(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = n; i >= 1; i--) {        // rows
+            for (int j = n; j >= 1; j--) {    // numbers
+                for (int k = 1; k <= i; k++) {
+                    list.add(j);
+                }
+            }
+            list.add(-1); // row separator
+        }
+
+        return list;
+    }
+}
