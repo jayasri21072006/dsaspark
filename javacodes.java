14dda1c1543e0e28e57869929dd00e98ed796bad
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-28 19:31:31 +0530

    Update javacodes.java

--- javacodes.java
+++ javacodes.java
@@ -245,3 +245,46 @@ class Main {
         System.out.println(Arrays.toString(arr1));
     }
 }
+
+
+
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem
+
+
+
+
+
+
+
+
+
+
+    class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];              // expand
+
+            while (sum >= target) {          // shrink
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}
+
