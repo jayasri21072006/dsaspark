e4a18e90ffef535c560110b7079d0f5eba266d80
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-28 19:33:01 +0530

    Update javacodes.java

--- javacodes.java
+++ javacodes.java
@@ -247,6 +247,7 @@ class Main {
 }
 
 
+SLIDING WINDOW
 
 
 Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
